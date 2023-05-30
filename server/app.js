259a96ff2dea9c12b2c047c08ed20e2ac9776895
Vishal Sarkar <vishalsarkar018@gmail.com>
2023-05-30 22:59:14 +0530

    Deploypath edited

--- server/app.js
+++ server/app.js
@@ -14,17 +14,17 @@ const authMiddleware = require("./middleware/auth");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 const notFound = require("./middleware/notFound");
 const path = require("path");
+const { log } = require("console");
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/chats", authMiddleware, chatRoutes);
 app.use("/api/v1/msgs", authMiddleware, messageRoutes);
 
-
 //_______For Deployement_________
-const _dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(_dirname, "/client/build")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(_dirname, "client", "build", "index.html"));
+    console.log(__dirname);
+    res.sendFile(path.resolve(__dirname, "..","client", "build", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
